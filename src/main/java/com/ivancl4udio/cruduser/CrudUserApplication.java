27caeb1e276b1383c4fff45a81630b3da851dab7
Ivan Claudio <ivan.claudio@gmail.com>
2022-09-12 14:13:11 -0300

    first commit

---
+++ src/main/java/com/ivancl4udio/cruduser/CrudUserApplication.java
@@ -0,0 +1,13 @@
+package com.ivancl4udio.cruduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivancl4udio/cruduser/CrudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivancl4udio.cruduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
